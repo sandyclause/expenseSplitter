1d6263b085fdbcf2870273ce4929e822f89d9b64
sandy tian <sandytian2000@hotmail.com>
2021-10-27 19:44:34 -0400

    amounts divided

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,16 @@
   <h1>test</h1>
   <router-link to="/people">Add people</router-link>
   <router-link to="/expense">Add expense</router-link>
+  <ResultsPage />
 </template>
 
 <script>
+import ResultsPage from '@/views/ResultsPage'
 
 export default {
   name: 'Home',
   components: {
+    ResultsPage
   }
 }
 </script>
---
+++ src/views/ResultsPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>Results</h2>
+
+  <div
+    v-for="(person, key) in people"
+    :key="key"
+  >
+    <p>{{ person.name }}</p>
+    <p
+      v-for="(expense, _key) in person.expenses"
+      :key="_key"
+    >
+      {{ expense.name }} ${{expense.amount}}
+    </p>
+    <p>total cost: ${{ totalCost(person.expenses) }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Results',
+  methods: {
+    totalCost(expenses) {
+      console.log(expenses)
+
+      const total = expenses.reduce((acc, expense) => {
+        const expenseAmount = expense.amount;
+        return acc + expenseAmount
+      }, 0)
+
+      return total
+    }
+  },
+  computed: {
+    people() {
+      return this.$store.getters.people
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
